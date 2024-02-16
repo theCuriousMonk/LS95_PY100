d0d8f3d087290759a8121548de4c95f1c18af57a
Rajanshu Ujjwal <rajanshu.ujjwal@gmail.com>
2024-02-16 10:46:08 +0530

    python basics

---
+++ hello.py
@@ -0,0 +1,35 @@
+maa = "Jai Maa Saraswati"
+def vasantpanchmi(name):
+    return ("Shubh Vasant Panchami, " + name)
+
+print (vasantpanchmi(maa))
+
+print("Hello Sunny")
+num1 = 10
+num2 = 20
+print(num1 + num2)
+print (max(10, 12))
+print (min(20, 21))
+name = "sunny"
+number = 161
+rate_of_interest = 1.0
+print (name)
+print (number)
+print (rate_of_interest)
+#this is a comment in python
+def abs(number):
+    if number >= 0:
+        return number
+    else:
+        return -number
+
+print(abs(-20))
+print(abs(-100))
+print(abs(393))
+
+#function to print name
+def myname(name):
+    return name
+
+print(myname("sunny"))
+print(myname("malt"))
